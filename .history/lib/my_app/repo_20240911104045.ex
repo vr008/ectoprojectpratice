555c380ff3538941af8a9e4e1670415c8130250a
vr008 <vigneshrajtsr@gmail.com>
2024-09-11 10:43:59 +0530

    new changes

--- .history/lib/my_app/repo_20240911104045.ex
+++ .history/lib/my_app/repo_20240911104045.ex
@@ -3,5 +3,7 @@ defmodule MusicDB.Repo do
   use Ecto.Repo,
     otp_app: :my_app,
     adapter: Ecto.Adapters.Postgres
-  
+    #def using_postgres? do
+    #  __MODULE__.config()[:adapter] == Ecto.Adapters.Postgres
+    #end
 end
